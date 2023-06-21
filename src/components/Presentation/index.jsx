b3ddc39ad9571be0036bcdcbc7d5042bf10578f8
Osmolite <trabuoh@gmail.com>
2023-06-21 18:21:12 +0200

    add useEffect logementCourant

--- src/components/Presentation/index.jsx
+++ src/components/Presentation/index.jsx
@@ -1,14 +1,24 @@
 import Info from '../logement_information'
 import logements from '../../datas/logements.json'
-import { useParams } from 'react-router-dom'
+import { json, useParams } from 'react-router-dom'
+import { useState } from 'react'
+import { useEffect } from 'react'
 //Contient Slideshow, Tag ainsi que le titre, la localisation du logement et Owner dans la page Product
 
 function Presentation() {
   let { id } = useParams()
-  const logementCourant = logements.find((element) => element.id === id)
+  const [logementCourant, setLogementCourant] = useState(null)
+  useEffect(()=> {
+    const monLogement = logements.find((element) => element.id === id)
+    setLogementCourant(monLogement)
+  },[])
+  // const logementCourant = logements.find((element) => element.id === id)
   if (typeof logementCourant==='undefined') {
     window.location.href='/ErrorPage'
   }
+  if (logementCourant==null) {
+    return <div></div>
+  } else {
   return (
     <Info
       title={logementCourant.title}
@@ -20,6 +30,6 @@ function Presentation() {
       equipments={logementCourant.equipments.join("\n")}
       pictures={logementCourant.pictures}
     ></Info>
-  )
+  )}
 }
 export default Presentation
